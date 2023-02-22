e1a29b1c6bbc36a6d7e1cf308711db7ab8e8128e
Sachin <sachinksahu.431@gmail.com>
2023-02-22 15:53:31 +0530

    Update code

--- hubconf.py
+++ hubconf.py
@@ -4,7 +4,9 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+# Get cpu or gpu device for training.
 device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
 
 classes = [
     "T-shirt/top",
@@ -19,34 +21,7 @@ classes = [
     "Ankle boot",
 ]
 
-def load_data():
-  training_data = datasets.FashionMNIST(
-    root='data',
-    train=True,
-    download=True,
-    transform=ToTensor(),
-  )
-
-  test_data = datasets.FashionMNIST(
-    root='data',
-    train=False,
-    download=True,
-    transform=ToTensor(),
-  )
-
-  return training_data, test_data
-
-def create_dataloaders(training_data, test_data, batch_size=64):
-  train_dataloader = DataLoader(training_data, batch_size=batch_size)
-  test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
-  for X, y in test_dataloader:
-    print(f'Shape of X[N, C, H, W: {X.shape}')
-    print(f'Shape of y: {y.shape} {y.dtype}')
-    break
-  return train_dataloader, test_dataloader
-
-# Neural Network
+# Define model
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
@@ -56,9 +31,7 @@ class NeuralNetwork(nn.Module):
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(512, 20),
-            nn.ReLU(),
-            nn.Linear(20, 10) 
+            nn.Linear(512, 10)
         )
 
     def forward(self, x):
@@ -66,14 +39,57 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
-def get_lossfn_and_optimizer(model):
+#############################
+
+def get_lossfn_and_optimizer(mymodel):
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.SGD(mymodel.parameters(), lr=1e-3)
     return loss_fn, optimizer
 
+
+def load_data():
+
+    # Download training data from open datasets.
+    training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    # Download test data from open datasets.
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+    
+    return training_data, test_data
+
+#############################
+
+def create_dataloaders(training_data, test_data, batch_size=64):
+
+    # Create data loaders.
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    for X, y in test_dataloader:
+        print(f"Shape of X [N, C, H, W]: {X.shape}")
+        print(f"Shape of y: {y.shape} {y.dtype}")
+        break
+        
+    return train_dataloader, test_dataloader
+  
+#############################
+
 def get_model():
+    
     model = NeuralNetwork().to(device)
+
     return model
+
 
 def _train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -93,11 +109,10 @@ def _train(dataloader, model, loss_fn, optimizer):
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
+            
 def _test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
-
     model.eval()
     test_loss, correct = 0, 0
     with torch.no_grad():
@@ -106,22 +121,20 @@ def _test(dataloader, model, loss_fn):
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    
     test_loss /= num_batches
     correct /= size
-
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-
-def train(train_dataloader, test_dataloader, model, loss_fn, optimizer, epochs=5):
+    
+def train(train_dataloader, test_dataloader, model1, loss_fn1, optimizer1, epochs=5):
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
-        _train(train_dataloader, model, loss_fn, optimizer)
-        _test(test_dataloader, model, loss_fn)
+        _train(train_dataloader, model1, loss_fn1, optimizer1)
+        _test(test_dataloader, model1, loss_fn1)
     print("Done!")
-    return model
+    return model1
 
-def save_model(model,mypath="model.pth"):
-    torch.save(model.state_dict(), "model.pth")
+def save_model(model1,mypath="model.pth"):
+    torch.save(model1.state_dict(), "model.pth")
     print("Saved PyTorch Model State to model.pth")
 
 def load_model(mypath="model.pth"):
@@ -129,10 +142,11 @@ def load_model(mypath="model.pth"):
     model.load_state_dict(torch.load("model.pth"))
     return model
 
-def sample_test(model, test_data):
-    model.eval()
+
+def sample_test(model1, test_data):
+    model1.eval()
     x, y = test_data[0][0], test_data[0][1]
     with torch.no_grad():
-        pred = model(x)
+        pred = model1(x)
         predicted, actual = classes[pred[0].argmax(0)], classes[y]
         print(f'Predicted: "{predicted}", Actual: "{actual}"')
